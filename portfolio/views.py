89e3126b54ef1d50476164b261a86f017c5c088d
Aayush3014 <ayushak3008@gmail.com>
2024-01-04 17:18:29 +0530

    Making Skills Section Dynamic

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from .models import ProjectModel
+from .models import ProjectModel, SkillsModel, SkillsTagModel
 # Create your views here.
 
 
 def homePage(request):
     projects = ProjectModel.objects.all()
-    return render(request, 'portfolio/home.html', {'projects':projects})
+    detailedSkills = SkillsModel.objects.exclude(skill_description="")
+    skills = SkillsModel.objects.filter(skill_description="")
+    context = {
+        'projects':projects,
+        'skills':skills,
+        'detailedSkills':detailedSkills,
+    }
+    print(context)
+    return render(request, 'portfolio/home.html', context)
