7279d954732b6ee422f8d9756897338ee2ffbd37
Aayush3014 <ayushak3008@gmail.com>
2024-01-08 22:19:30 +0530

    Add Project Feature Completed

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,4 +5,39 @@ from .models import ProjectModel
 class ProjectForm(ModelForm):
     class Meta:
         model = ProjectModel
-        fields = '__all__'
+        fields = ['project_title', 'project_thumbnail', 'project_description']
+
+    def __init__(self, *args, **kwargs):
+        
+        """The __init__ method is called when an instance of the ProjectForm class is created. 
+            The super call ensures that the constructor of the parent class (ModelForm) is also executed. 
+            Following that, it customizes the appearance of the form fields by updating the widget attributes 
+            for 'project_title' and 'project_description' fields, adding a 'form-control' class."""
+            
+            
+        super(ProjectForm, self).__init__(*args, **kwargs)
+            
+        self.fields['project_title'].widget.attrs.update(
+            {'class': 'form-control'})
+        
+        """
+            self.fields['project_title']: This part refers to the 'project_title' field within the form. In Django forms, 
+            each field is represented as an instance of a field class, and you can access them using dictionary-like syntax
+            on the fields attribute of the form.
+
+            
+            .widget: The .widget attribute of a form field refers to the HTML widget used to render that field in an HTML form. 
+            The widget determines how the field is presented on the web page.
+
+            
+            .attrs: This is an attribute of the widget that holds a dictionary of HTML attributes for that specific widget.
+
+            
+            .update({'class': 'form-control'}): This method updates the dictionary of attributes for the widget.
+            In this case, it's adding a new attribute 'class' with the value 'form-control'. 
+            The 'form-control' class is commonly used in web development, especially with CSS frameworks like Bootstrap,
+            to style form elements and make them visually consistent.
+        """
+
+        self.fields['project_description'].widget.attrs.update(
+            {'class': 'form-control', })
