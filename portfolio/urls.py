8fe88e286b531e396951b6f7d4c6a79fcd2cc3ef
Aayush3014 <ayushak3008@gmail.com>
2024-01-08 22:35:48 +0530

    Edit Project Feature Completed

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homePage, projectpage, addProject
+from .views import homePage, projectpage, addProject, editProject
 
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', homePage, name='home'),
     path('project/<str:pk>/', projectpage, name='project'),
     path('add-project/', addProject, name='add-project'),
+    path('edit-project/<str:pk>', editProject, name='edit-project'),
 ]
